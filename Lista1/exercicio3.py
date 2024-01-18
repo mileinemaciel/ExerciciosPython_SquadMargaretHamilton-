5e9def6203540a660705cbba899697f1fed940ce
PuriNakayama <priscila.nakayama@yahoo.com>
2024-01-18 19:56:17 -0300

    exercicio1

--- Lista1/exercicio3.py
+++ Lista1/exercicio3.py
@@ -11,6 +11,7 @@ distancia_km = float(input('Digite a quantidade de quilômetros: '))
 distancia_m, distancia_cm, distancia_mm = calcula_unidades(distancia_km)
 
 #imprime a distancia em metros, centimetros, milimetros
+
 print( f'A distância em metros é: {distancia_m}')
 print( f'A distância em centímetros é: {distancia_cm}' )
 print( f'A distância em milímetros é: {distancia_mm}' )
