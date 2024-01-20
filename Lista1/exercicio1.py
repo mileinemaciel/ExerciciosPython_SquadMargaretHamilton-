51c428c25ea2516640d5feadcaeeba9218a7cbe0
Marcella Aguiar de Almeida Amazonas Amazonas <102737277+marcellaamazonas@users.noreply.github.com>
2024-01-20 14:50:48 -0300

    Create exercicio1.py
    Resolução do exercício 1 da Lista 1.

---
+++ Lista1/exercicio1.py
@@ -0,0 +1,21 @@
+# Faça um Programa que peça dois números,realize as principais operações soma, subtração, multiplicação, divisão.
+
+numero1 = float(input('Digite o primeiro número: '))
+numero2 = float(input('Digite o segundo número: '))
+
+
+# soma
+soma = numero1 + numero2
+print(f'A soma de {numero1} com {numero2} é {soma}')
+
+# subtração
+subtracao = numero1 - numero2
+print(f'A subtração de {numero1} com {numero2} é {subtracao}')
+
+#multiplicação
+multiplicacao = numero1 * numero2
+print(f'A multiplicação de {numero1} por {numero2} é {multiplicacao}')
+
+# divisão
+divisao = numero1 / numero2
+print(f'A divisão de {numero1} por {numero2} é {divisao}')
