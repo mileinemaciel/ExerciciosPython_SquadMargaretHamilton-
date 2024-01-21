a2ad5403381b7ab2fbff02de0d5dd399c0ce18a5
mileinemaciel <150167181+mileinemaciel@users.noreply.github.com>
2024-01-21 14:39:21 -0300

    Create Exercicio 1.py

---
+++ Lista2/Exercicio 1.py
@@ -0,0 +1,15 @@
+# Exercicio 1
+
+#Faça um Programa que peça dois números e imprima o maior deles
+
+
+numero1 = int(input('Digite o primeiro número: '))
+numero2 = int(input('Digite o segundo número: '))
+
+
+if numero1 > numero2:
+    print(f'O maior número é: {numero1}')
+
+else:
+    numero2 > numero1
+    print(f'O maior número é: {numero2}')
